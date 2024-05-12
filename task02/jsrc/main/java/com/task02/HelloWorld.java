5089c8e52957c095d7edb671600f6259c46411ee
Samir_Allahverdiyev <smrallahverdieff@gmail.com>
2024-05-12 19:39:32 +0200

    task02

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +14,7 @@ import java.util.Map;
 	roleName = "hello_world-role",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@LambdaUrlConfig
+@LambdaUrlConfig(authType = AuthType.NONE)
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
@@ -24,13 +25,19 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
 		if(path.equals("/hello")){
 			resultMap.put("statusCode", 200);
-			resultMap.put("message", "Hello from Lambda");
+			resultMap.put("body", "{" +
+					"    \"statusCode\": 200," +
+					"    \"message\": \"Hello from Lambda\"" +
+					"}");
 		} else {
 			Map<String,Object> requestContext = (Map<String,Object>) mapRequst.get("requestContext");
 			Map<String,Object> http = (Map<String,Object>) requestContext.get("http");
 			String method = (String) http.get("method");
 			resultMap.put("statusCode", 400);
-			resultMap.put("message", "Bad request syntax or unsupported method. Request path: "+ path + ". HTTP method: " + method);
+			resultMap.put("body", "{" +
+							"    'statusCode': 400," +
+							"    'message': 'Bad request syntax or unsupported method. Request path: " + path + ". HTTP method: " + method+ "'" +
+							"}" );
 		}
 
 	   return resultMap;
